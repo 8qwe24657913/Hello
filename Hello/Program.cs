ebc5899fa9582e9063111abea6c1d55190291fbd
8q <yggdfolium@gmail.com>
2019-03-03 23:37:01 +0800

    update

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -7,13 +7,18 @@ using System.Threading.Tasks;
 namespace Hello {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            if (args.Length >= 2) {
+                System.Console.WriteLine("Hello World!{0} is {1}", args[0], args[1]);
+            } else {
+                System.Console.WriteLine("Hello World! command line parameter does not meet the requirement!");
+            }
             using (new ChangeColor.Back(ConsoleColor.Green)) {
                 Console.WriteLine("Hello World!");
                 Console.WriteLine("Hello World!");
                 using (new ChangeColor.Fore(ConsoleColor.Yellow)) {
                     Console.WriteLine("Hello World!");
                 }
+                Console.WriteLine("If you use ResetColor(), you will have to reassign background color here.");
             }
         }
     }
